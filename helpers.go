e9a0c7dc26ab68105d2167f8ed51687bcda7244d
Galdoba <galdoba@mail.ru>
2019-06-14 19:13:20 +1000

    AppendUnique - INT or STRING

--- helpers.go
+++ helpers.go
@@ -16,6 +16,14 @@ func CheckError(descr string, err error) string {
 //AppendUnique - проверяет есть ли новый элемент в слайсе.
 //Если нет, то добавляет его в слайс. В противном случае возвращает слайс без изменений.
 func AppendUnique(slice []interface{}, newElem interface{}) []interface{} {
+	switch newElem.(type) {
+	default:
+		panic(fmt.Sprintf("invalid type %T (myLib)", newElem))
+	case int, int64:
+
+	case string:
+
+	}
 	for i := range slice {
 		if slice[i] == newElem {
 			return slice
