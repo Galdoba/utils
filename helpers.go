c62282c9ea1ee25d586efc6a2237ae496aac8150
Galdoba <galdoba@mail.ru>
2019-06-14 18:39:50 +1000

    AppendUnique

--- helpers.go
+++ helpers.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"go/types"
 )
 
 func CheckError(descr string, err error) string {
@@ -11,4 +12,16 @@ func CheckError(descr string, err error) string {
 		panic(message)
 	}
 	return message
+}
+
+//AppendUnique - проверяет есть ли новый элемент в слайсе.
+//Если нет, то добавляет его в слайс. В противном случае возвращает слайс без изменений.
+func AppendUnique(slice []types.Type, newElem types.Type) []types.Type {
+	for i := range slice {
+		if slice[i] == newElem {
+			return slice
+		}
+	}
+	slice = append(slice, newElem)
+	return slice
 }
