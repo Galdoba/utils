36f2d3cb73b83ce2d9038f9e6fdd0beb77894cf8
Galdoba <galdoba@mail.ru>
2019-07-15 19:32:18 +1000

    PickFewUniqueFromList

--- helpers.go
+++ helpers.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func CheckError(descr string, err error) string {
@@ -23,6 +24,26 @@ func AppendUniqueStr(slice []string, newElem string) []string {
 	}
 	slice = append(slice, newElem)
 	return slice
+}
+
+//PickFewUniqueFromList - выбирает из списка несколько случайных позиций
+//если длинна списка меньше n возвращает весь список в случайном порядке
+func PickFewUniqueFromList(list []string, n int) []string {
+	var result []string
+	if n < 1 {
+		return result
+	}
+	if len(list) < n {
+		for i := range list {
+			j := rand.Intn(i + 1)
+			list[i], list[j] = list[j], list[i]
+		}
+		return list
+	}
+	for len(result) < n {
+		result = AppendUniqueStr(result, RandomFromList(list))
+	}
+	return result
 }
 
 //AppendUniqueInt - проверяет есть ли новый элемент в слайсе.
