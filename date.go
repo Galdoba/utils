883e12d2069a62ca3dd7f708469eb1bd25b183b9
Galdoba <galdoba@mail.ru>
2021-04-06 15:53:22 +1000

    Duration

--- date.go
+++ date.go
@@ -19,3 +19,37 @@ func DateStamp() string {
 	}
 	return yy + "-" + mm + "-" + dd
 }
+
+//DurationStamp - return duration (float64 - seconds) as a string in format: [HH:MM:SS.ms]
+func DurationStamp(dur float64) string {
+	if dur < 0 {
+		return "--NEGATIVE--"
+	}
+	stamp := ""
+	hh := int(dur) / int(3600)
+	hLeft := int(dur) % int(3600)
+	mm := hLeft / 60
+	ss := hLeft % 60
+	sLeft := dur - (float64(hh*3600) + float64(mm*60) + float64(ss))
+	ms := int(sLeft * 1000)
+	////////
+	hhs := strconv.Itoa(int(hh))
+	for len(hhs) < 2 {
+		hhs = "0" + hhs
+	}
+	mms := strconv.Itoa(int(mm))
+	for len(mms) < 2 {
+		mms = "0" + mms
+	}
+	sss := strconv.Itoa(int(ss))
+	for len(sss) < 2 {
+		sss = "0" + sss
+	}
+	mss := strconv.Itoa(int(ms))
+	for len(mss) < 3 {
+		mss = "0" + mss
+	}
+	stamp = hhs + ":" + mms + ":" + sss + "." + mss
+	return stamp
+
+}
